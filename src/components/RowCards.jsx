6e32d85ae06adc4ca2a08837787626e6ead6311b
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-04 19:24:05 -0300

    logica terminada

--- src/components/RowCards.jsx
+++ src/components/RowCards.jsx
@@ -2,7 +2,7 @@ import {  Row } from "react-bootstrap";
 import Cards from "./Cards";
 const RowCards = ({noticiasFiltradas}) => {
   return (
-      <Row className="gy-3">
+      <Row className="gy-3 justify-content-center">
         {noticiasFiltradas.map(objetoNoticia => <Cards objetoNoticia={objetoNoticia}></Cards> )}
       </Row>
   );
