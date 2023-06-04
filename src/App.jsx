015f25c60e42267d2e11a8f6e95865e373e08b01
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-04 16:59:12 -0300

    se crea componente para titulo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AdministradorNoticias from "./components/AdministradorNoticias";
+import Titulo from "./components/Titulo";
 
 function App() {
   return (
     <>
       <Container className="py-3 px-md-5 main">
-        <h1 className="mt-4 text-center">Breaking news</h1>
+        <Titulo></Titulo>
         <AdministradorNoticias></AdministradorNoticias>
       </Container>
       <footer className="py-4 bg-black text-light text-center">
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,5 @@
+const Titulo = () => {
+  return <h1 className="mt-4 text-center">Breaking news</h1>;
+};
+
+export default Titulo;
