fa3bc38cdeffbd592016722d1ddf25632e41ae69
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-06 16:54:59 -0300

    se actualiza idioma

--- src/components/AdministradorNoticias.jsx
+++ src/components/AdministradorNoticias.jsx
@@ -13,13 +13,17 @@ const AdministradorNoticias = () => {
 
   const consultarAPI = async () => {
     try {
-        const respuesta = await fetch("https://newsdata.io/api/1/news?apikey=pub_240135ddcbf2e44d1a628028e9bb6a82d03a4&q=ukraine");
-        const informacion = await respuesta.json();
-        console.log(respuesta);
-        setNoticias(informacion.results);
-        console.log(noticias);
-        const nFiltradas = noticias.filter((e)=>e.category[0] === categoriaSeleccionada)
-        setNoticiasFiltradas(nFiltradas);
+      const respuesta = await fetch(
+        "https://newsdata.io/api/1/news?apikey=pub_240135ddcbf2e44d1a628028e9bb6a82d03a4&country=ar"
+      );
+      const informacion = await respuesta.json();
+      console.log(respuesta);
+      setNoticias(informacion.results);
+      console.log(noticias);
+      const nFiltradas = noticias.filter(
+        (e) => e.category[0] === categoriaSeleccionada
+      );
+      setNoticiasFiltradas(nFiltradas);
     } catch (error) {
       console.log(error);
     }
@@ -45,10 +49,10 @@ const AdministradorNoticias = () => {
                     setCategoriaSeleccionada(e.target.value);
                   }}
                 >
-                  <option value="all">select by category</option>
-                  <option value="top">Mundial</option>
-                  <option value="business">Business</option>
-                  <option value="politics">Politics</option>
+                  <option value="top">Selecciona una categoría</option>
+                  <option value="science">Ciencia</option>
+                  <option value="sports">Deportes</option>
+                  <option value="business">Negocios</option>
                 </Form.Select>
               </Col>
             </Form.Group>
